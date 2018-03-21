3a3197c9348f4ea7ce897ebf917a194b40e932ec
Chris Cooper <cooper.c@ufl.edu>
2018-03-21 15:38:06 -0400

    Trial 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "main.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    test(13,12);
     return 0;
+}
+
+void test(int a, int b ){
+    if ( a > b){
+        std::cout<<"It's bigger.";
+    }
 }
